703665739cb09b9676f5af84198dcf5304c5854b
Joshua Head <joshua.head@outlook.com>
2014-02-07 23:52:11 +1000

    fixed server and added somthing great

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ set :branch, 'master'
 set :deploy_to, '/home/aagriff/apps/webApp_appsAtgriffith'
 set :scm, :git
 
+
 set :user, "aagriff"
 set :group, "deploy-group"
 set :use_sudo, false
@@ -17,21 +18,37 @@ set :format, :pretty
 set :log_level, :debug
 set :pty, true
 
-set :linked_files, %w{config/mongoid.yml initializers/devise.yml}
+set :linked_files, %w{config/mongoid.yml config/initializers/devise.rb}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
  set :keep_releases, 5
 
+set :filter, :hosts => %w{162.248.164.142}
+
 namespace :deploy do
 
-  on hosts do |host|
-      upload! '/config/database.yml', '#{shared_path}/databse.yml'
+
+  desc "Update the sharedStuff"
+  task :upload_yml_files do
+    on roles(:all) do |host|
+      upload! 'config/mongoid.yml',  "#{shared_path}/config/mongoid.yml"
+      upload! 'config/initializers/devise.rb', "#{shared_path}/config/initializers/devise.rb"
+    end
   end
 
-  on hosts do |host|
-      upload! '/config/initializers.yml', '#{shared_path}/initializers/devise.yml'
+  desc "Check that we can access everything"
+  task :check_write_permissions do
+    on roles(:all) do |host|
+      if test("[ -w #{fetch(:deploy_to)} ]")
+        info "#{fetch(:deploy_to)} is writable on #{host}"
+      else
+        error "#{fetch(:deploy_to)} is not writable on #{host}"
+      end
+    end
   end
+
+  #before :started, 'deploy:upload_yml_files'
 
   desc 'Restart application'
   task :restart do
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ WebApp::Application.routes.draw do
   devise_for :users
   root :to => "home#index"
   get "login/index"
+  resources :application
   resources :login
 
   # The priority is based upon order of creation: first created -> highest priority.
