013fbaf77e34088f938e2dbcf5722f764e914efd
Joshua Head <joshua.head@outlook.com>
2014-02-09 00:06:28 +1000

    user has to be logged in to edit/delete a project, also fixed a regressed bug due to me commenting out some of the model. but YAYYYYY

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
     before_action :set_product, only: [:show, :edit, :update, :destroy]
-
+    before_action :authenticate_user!, only: [:edit, :update, :destory]
     def index
         @projects = Project.all
     end
@@ -27,6 +27,26 @@ class ProjectsController < ApplicationController
                 format.html { render action: 'new' }
                 format.json { render json: @project.errors, status: :unprocessable_entity }
             end
+        end
+    end
+
+    def update
+        respond_to do |format|
+          if @project.update(project_params)
+            format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+            format.json { head :no_content }
+          else
+            format.html { render action: 'edit' }
+            format.json { render json: @project.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+    def destroy
+        @project.destroy
+            respond_to do |format|
+            format.html { redirect_to projects_url }
+            format.json { head :no_content }
         end
     end
 
